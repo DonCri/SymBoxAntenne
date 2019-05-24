3912d4c2d52674a58d082ba0b7140f7e84447c2b
DonCri <36209368+DonCri@users.noreply.github.com>
2019-05-24 19:34:09 +0200

    Add files via upload

--- SymAnRohdaten/module.php
+++ SymAnRohdaten/module.php
@@ -48,7 +48,7 @@ class SymAnRohdaten extends IPSModule {
 	    $data = json_decode($JSONString);
 
 	    // Datenverarbeitung und schreiben der Werte in die Statusvariablen
-		SetValue($this->GetIDForIdent("eGate"), base_convert(print_r($data->Buffer, true), 8, 16));
+		SetValue($this->GetIDForIdent("eGate"), bin2hex(print_r($data->Buffer, true));
 
 	}
 
