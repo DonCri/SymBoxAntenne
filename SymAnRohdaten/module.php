3ebdee440a01e196b1491fcdf70b3a41177c6c12
DonCri <36209368+DonCri@users.noreply.github.com>
2019-05-26 16:14:06 +0200

    Add files via upload

--- SymAnRohdaten/module.php
+++ SymAnRohdaten/module.php
@@ -59,12 +59,11 @@ class SymAnRohdaten extends IPSModule {
 	public function BefehlTest() {
 
 			$Value = GetValue($this->GetIDForIdent("Command"));
-			$Adresse = GetValue($this->ReadPropertyString("Adresse"));
 
 			switch($Value)
 			{
 				case true:
-					return $this->SendDataToParent(json_encode(Array("DataID" => "{79827379-F36E-4ADA-8A95-5F8D1DC92FA9}", "Buffer" => hex2bin("c2a0" . "01" . $Adresse . "0000" . "05"))));	
+					return $this->SendDataToParent(json_encode(Array("DataID" => "{79827379-F36E-4ADA-8A95-5F8D1DC92FA9}", "Buffer" => hex2bin("c2a0" . "01" . $this->ReadPropertyString("Adresse") . "0000" . "05"))));	
 				break;
 			}
 	
