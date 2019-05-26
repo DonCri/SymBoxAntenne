6c81c8e8db03d1010a4696c62262eb6cb0152c9f
DonCri <36209368+DonCri@users.noreply.github.com>
2019-05-26 21:14:05 +0200

    Add files via upload

--- SymAnRohdaten/module.php
+++ SymAnRohdaten/module.php
@@ -91,7 +91,7 @@ class SymAnRohdaten extends IPSModule {
         	case "Command":
 				//Neuen Wert in die Statusvariable schreiben
 					SetValue($this->GetIDForIdent($Ident), $Value);
-					$this->BefehlTest();	
+					$this->ForwardData();	
 			break;
 			case "lowerValueSun":
 				//Neuen Wert in die Statusvariable schreiben
