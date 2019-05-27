f68cc5497c325562cd7b32c59b771890797dff6f
DonCri <36209368+DonCri@users.noreply.github.com>
2019-05-28 00:59:48 +0200

    Add files via upload

--- SymAnRohdaten/module.php
+++ SymAnRohdaten/module.php
@@ -69,7 +69,7 @@ class SymAnRohdaten extends IPSModule {
 					switch($Button)
 					{
 						case true:
-							$FSSBefehl = hex2bin("01" . $this->ReadPropertyString("Adresse") . "0000" . "05" "00");
+							$FSSBefehl = hex2bin("01" . $this->ReadPropertyString("Adresse") . "0000" . "05" . "00");
 							SetValue($this->GetIDForIdent("eGate"), bin2hex($FSSBefehl));
 							return $this->SendDataToParent(json_encode(Array("DataID" => "{79827379-F36E-4ADA-8A95-5F8D1DC92FA9}", "Buffer" => $FSSBefehl)));	
 								
